7bef1320b1a26205347f10230d989b7629664254
mth0348 <matthias.thomann@hotmail.com>
2018-09-28 16:04:00 +0200

    Added a cardService class in a new file called cardService.php.

---
+++ php/cardService.php
@@ -0,0 +1,19 @@
+<?php
+    class CardService
+    {
+        private $baseUrl = 'https://omgvamp-hearthstone-v1.p.mashape.com/cards/'; 
+        private $authenticationArray = array('X-Mashape-Key: Law2aqNS0LmshaQuw94sPj0yVk0bp1Ni6AXjsnvgaylweSq7CG');
+
+        public function searchForCards($query)
+        {
+            $curlRequest = curl_init($this->baseUrl . "search/" . $query);
+
+            curl_setopt($curlRequest, CURLOPT_HTTPHEADER, $this->authenticationArray);
+            curl_setopt($curlRequest, CURLOPT_RETURNTRANSFER, true);
+
+            $response = curl_exec($curlRequest);
+
+            return $response;
+        }
+    }
+?>
