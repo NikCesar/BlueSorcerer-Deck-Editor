656d97be43842060d303aa1525fad81a2441940b
mth0348 <matthias.thomann@hotmail.com>
2018-10-01 19:00:30 +0200

    added dummy deck editor page (empty).

---
+++ pages/deckEditor.php
@@ -0,0 +1,17 @@
+<!doctype html>
+
+<html>
+    <head>
+        <?php include "partials/_scripts.php"; ?>
+    </head>
+
+    <body>
+        <?php include "partials/_topBar.php"; ?>
+
+        <div class="content">
+            <p>TODO</p>
+        </div>
+
+        <footer></footer>
+    </body>
+</html>
