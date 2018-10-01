5dd5913ed6931ce1305a842845f02cc5e57416c1
mth0348 <matthias.thomann@hotmail.com>
2018-10-01 18:54:25 +0200

    fixed some css classes and removed unnecessary div containers.

--- pages/login.php
+++ pages/login.php
@@ -8,28 +8,26 @@
     <body>
         <?php include "partials/_topBar.php"; ?>
 
-        <div class="content">
-            <div id="loginWindow">
-                <div id="credentials">
-                    <section id="username">
-                        <label id="usernameLabel">Username: </label>
-                        <input id="usernameTextArea">
-                    </section>
+        <div class="content small-content">
+            <div class="centered">
+                <section id="username">
+                    <label id="usernameLabel">Username: </label>
+                    <input id="usernameTextArea">
+                </section>
 
-                    <section id="password">
-                        <label id="pwLabel">Password: </label>
-                        <input id="pw" type="password">
-                    </section>
-                    <button id="loginButton">Login</button>
+                <section id="password">
+                    <label id="pwLabel">Password: </label>
+                    <input id="pw" type="password">
+                </section>
+                <button id="loginButton">Login</button>
+            </div>
+
+            <div id="register">
+                <div>
+                    <a href="#">Forgot Password?</a>
                 </div>
-
-                <div id="register">
-                    <div>
-                        <a href="#">Forgot Password?</a>
-                    </div>
-                    <div>
-                        <a href="#">No Account? Register now!</a>
-                    </div>
+                <div>
+                    <a href="#">No Account? Register now!</a>
                 </div>
             </div>
         </div>
