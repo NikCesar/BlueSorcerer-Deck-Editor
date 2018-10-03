9255ff4273c12262ed9be837a7ea4df72237f379
mth0348 <matthias.thomann@hotmail.com>
2018-10-03 18:36:46 +0200

    Created SCSS files out of CSS files.

--- pages/partials/_scripts.php
+++ pages/partials/_scripts.php
@@ -1,6 +1,6 @@
 <title>BlueSorcerer - 4 * your deckbuilding</title>
 <link rel="stylesheet" href="https://meyerweb.com/eric/tools/css/reset/reset200802.css">
-<link rel="stylesheet" href="http://<?php echo $_SERVER['SERVER_NAME']; ?>/assets/css/root.css">
+<link rel="stylesheet" href="http://<?php echo $_SERVER['SERVER_NAME']; ?>/assets/css/_out/styles.css">
 
 <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
 <script src="http://<?php echo $_SERVER['SERVER_NAME']; ?>/assets/js/script.js"></script>
