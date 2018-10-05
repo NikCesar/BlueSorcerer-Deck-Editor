69a2e9ff9e7f2ae57c4e790fe0f0be0e7fde6468
mth0348 <matthias.thomann@hotmail.com>
2018-10-05 10:19:46 +0200

    fixed userProfile.php.

--- pages/userProfile.php
+++ pages/userProfile.php
@@ -2,33 +2,11 @@
 
 <html>
     <head>
-
-        <title>BlueSorcerer - 4 * your deckbuilding</title>
-        <link rel="stylesheet" href="https://meyerweb.com/eric/tools/css/reset/reset200802.css">
-        <link rel="stylesheet" href="../assets/css/root.css">
-
+        <?php include "partials/_scripts.php"; ?>
     </head>
 
     <body>
-
-        <div id="topBar">
-            <header>
-                <h1>Blue Sorcerer</h1>
-                <h3>Deck Editor</h3>
-                <img id="headerImage" src="../assets/img/sorcerer_header.png" />
-            </header>
-
-            <nav>
-                <ul>
-                    <li><a href="../index.php">Home</a></li>
-                    <li><a href="#">Link 2</a></li>
-                    <li><a href="#">Link 3</a></li>
-                    <li><a href="#">Link 4</a></li>
-                    <li id="loginItem"><a href="#">Login</a></li>
-                </ul>
-                <div style="clear: both"></div>
-            </nav>
-        </div>
+        <?php include "partials/_topBar.php"; ?>
 
         <div class="content">
             <section id="profile">
