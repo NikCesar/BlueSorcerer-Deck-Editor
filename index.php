ccb6cbcba5d5d7c0a796f30111bb05a1c80dbe0c
mth0348 <matthias.thomann@hotmail.com>
2018-10-05 14:20:37 +0200

    added small styling to deck listings in index.php.

--- index.php
+++ index.php
@@ -14,112 +14,112 @@
             <section id="newestDecks">
                 <input type="text" onkeyup="searchForCards(this.value);" />
 
-                <div>Deck 1</div>
-                <div>Deck 2</div>
-                <div>Deck 3</div>
-                <div>Deck 4</div>
-                <div>Deck 5</div>
-                <div>Deck 6</div>
-                <div>Deck 7</div>
-                <div>Deck 8</div>
-                <div>Deck 9</div>
-                <div>Deck 10</div>
-                <div>Deck 1</div>
-                <div>Deck 2</div>
-                <div>Deck 3</div>
-                <div>Deck 4</div>
-                <div>Deck 5</div>
-                <div>Deck 6</div>
-                <div>Deck 7</div>
-                <div>Deck 8</div>
-                <div>Deck 9</div>
-                <div>Deck 10</div>
-                <div>Deck 5</div>
-                <div>Deck 6</div>
-                <div>Deck 7</div>
-                <div>Deck 8</div>
-                <div>Deck 9</div>
-                <div>Deck 10</div>
+                <div class="deck-listing">Deck 1</div>
+                <div class="deck-listing">Deck 2</div>
+                <div class="deck-listing">Deck 3</div>
+                <div class="deck-listing">Deck 4</div>
+                <div class="deck-listing">Deck 5</div>
+                <div class="deck-listing">Deck 6</div>
+                <div class="deck-listing">Deck 7</div>
+                <div class="deck-listing">Deck 8</div>
+                <div class="deck-listing">Deck 9</div>
+                <div class="deck-listing">Deck 10</div>
+                <div class="deck-listing">Deck 1</div>
+                <div class="deck-listing">Deck 2</div>
+                <div class="deck-listing">Deck 3</div>
+                <div class="deck-listing">Deck 4</div>
+                <div class="deck-listing">Deck 5</div>
+                <div class="deck-listing">Deck 6</div>
+                <div class="deck-listing">Deck 7</div>
+                <div class="deck-listing">Deck 8</div>
+                <div class="deck-listing">Deck 9</div>
+                <div class="deck-listing">Deck 10</div>
+                <div class="deck-listing">Deck 5</div>
+                <div class="deck-listing">Deck 6</div>
+                <div class="deck-listing">Deck 7</div>
+                <div class="deck-listing">Deck 8</div>
+                <div class="deck-listing">Deck 9</div>
+                <div class="deck-listing">Deck 10</div>
             </section>
     
             <section id="topDecks">
-                <div>Deck 1</div>
-                <div>Deck 2</div>
-                <div>Deck 3</div>
-                <div>Deck 4</div>
-                <div>Deck 5</div>
-                <div>Deck 6</div>
-                <div>Deck 7</div>
-                <div>Deck 8</div>
-                <div>Deck 9</div>
-                <div>Deck 10</div>
-                <div>Deck 1</div>
-                <div>Deck 2</div>
-                <div>Deck 3</div>
-                <div>Deck 4</div>
-                <div>Deck 5</div>
-                <div>Deck 6</div>
-                <div>Deck 7</div>
-                <div>Deck 8</div>
-                <div>Deck 9</div>
-                <div>Deck 10</div>
-                <div>Deck 2</div>
-                <div>Deck 3</div>
-                <div>Deck 4</div>
-                <div>Deck 5</div>
-                <div>Deck 6</div>
-                <div>Deck 7</div>
-                <div>Deck 8</div>
-                <div>Deck 9</div>
-                <div>Deck 10</div>
-                <div>Deck 1</div>
-                <div>Deck 2</div>
-                <div>Deck 3</div>
-                <div>Deck 4</div>
-                <div>Deck 5</div>
-                <div>Deck 6</div>
-                <div>Deck 7</div>
-                <div>Deck 8</div>
-                <div>Deck 9</div>
-                <div>Deck 10</div>
-                <div>Deck 2</div>
-                <div>Deck 3</div>
-                <div>Deck 4</div>
-                <div>Deck 5</div>
-                <div>Deck 6</div>
-                <div>Deck 7</div>
-                <div>Deck 8</div>
-                <div>Deck 9</div>
-                <div>Deck 10</div>
-                <div>Deck 1</div>
-                <div>Deck 2</div>
-                <div>Deck 3</div>
-                <div>Deck 4</div>
-                <div>Deck 5</div>
-                <div>Deck 6</div>
-                <div>Deck 7</div>
-                <div>Deck 8</div>
-                <div>Deck 9</div>
-                <div>Deck 10</div>
-                <div>Deck 2</div>
-                <div>Deck 3</div>
-                <div>Deck 4</div>
-                <div>Deck 5</div>
-                <div>Deck 6</div>
-                <div>Deck 7</div>
-                <div>Deck 8</div>
-                <div>Deck 9</div>
-                <div>Deck 10</div>
-                <div>Deck 1</div>
-                <div>Deck 2</div>
-                <div>Deck 3</div>
-                <div>Deck 4</div>
-                <div>Deck 5</div>
-                <div>Deck 6</div>
-                <div>Deck 7</div>
-                <div>Deck 8</div>
-                <div>Deck 9</div>
-                <div>Deck 10</div>
+                <div class="deck-listing">Deck 1</div>
+                <div class="deck-listing">Deck 2</div>
+                <div class="deck-listing">Deck 3</div>
+                <div class="deck-listing">Deck 4</div>
+                <div class="deck-listing">Deck 5</div>
+                <div class="deck-listing">Deck 6</div>
+                <div class="deck-listing">Deck 7</div>
+                <div class="deck-listing">Deck 8</div>
+                <div class="deck-listing">Deck 9</div>
+                <div class="deck-listing">Deck 10</div>
+                <div class="deck-listing">Deck 1</div>
+                <div class="deck-listing">Deck 2</div>
+                <div class="deck-listing">Deck 3</div>
+                <div class="deck-listing">Deck 4</div>
+                <div class="deck-listing">Deck 5</div>
+                <div class="deck-listing">Deck 6</div>
+                <div class="deck-listing">Deck 7</div>
+                <div class="deck-listing">Deck 8</div>
+                <div class="deck-listing">Deck 9</div>
+                <div class="deck-listing">Deck 10</div>
+                <div class="deck-listing">Deck 2</div>
+                <div class="deck-listing">Deck 3</div>
+                <div class="deck-listing">Deck 4</div>
+                <div class="deck-listing">Deck 5</div>
+                <div class="deck-listing">Deck 6</div>
+                <div class="deck-listing">Deck 7</div>
+                <div class="deck-listing">Deck 8</div>
+                <div class="deck-listing">Deck 9</div>
+                <div class="deck-listing">Deck 10</div>
+                <div class="deck-listing">Deck 1</div>
+                <div class="deck-listing">Deck 2</div>
+                <div class="deck-listing">Deck 3</div>
+                <div class="deck-listing">Deck 4</div>
+                <div class="deck-listing">Deck 5</div>
+                <div class="deck-listing">Deck 6</div>
+                <div class="deck-listing">Deck 7</div>
+                <div class="deck-listing">Deck 8</div>
+                <div class="deck-listing">Deck 9</div>
+                <div class="deck-listing">Deck 10</div>
+                <div class="deck-listing">Deck 2</div>
+                <div class="deck-listing">Deck 3</div>
+                <div class="deck-listing">Deck 4</div>
+                <div class="deck-listing">Deck 5</div>
+                <div class="deck-listing">Deck 6</div>
+                <div class="deck-listing">Deck 7</div>
+                <div class="deck-listing">Deck 8</div>
+                <div class="deck-listing">Deck 9</div>
+                <div class="deck-listing">Deck 10</div>
+                <div class="deck-listing">Deck 1</div>
+                <div class="deck-listing">Deck 2</div>
+                <div class="deck-listing">Deck 3</div>
+                <div class="deck-listing">Deck 4</div>
+                <div class="deck-listing">Deck 5</div>
+                <div class="deck-listing">Deck 6</div>
+                <div class="deck-listing">Deck 7</div>
+                <div class="deck-listing">Deck 8</div>
+                <div class="deck-listing">Deck 9</div>
+                <div class="deck-listing">Deck 10</div>
+                <div class="deck-listing">Deck 2</div>
+                <div class="deck-listing">Deck 3</div>
+                <div class="deck-listing">Deck 4</div>
+                <div class="deck-listing">Deck 5</div>
+                <div class="deck-listing">Deck 6</div>
+                <div class="deck-listing">Deck 7</div>
+                <div class="deck-listing">Deck 8</div>
+                <div class="deck-listing">Deck 9</div>
+                <div class="deck-listing">Deck 10</div>
+                <div class="deck-listing">Deck 1</div>
+                <div class="deck-listing">Deck 2</div>
+                <div class="deck-listing">Deck 3</div>
+                <div class="deck-listing">Deck 4</div>
+                <div class="deck-listing">Deck 5</div>
+                <div class="deck-listing">Deck 6</div>
+                <div class="deck-listing">Deck 7</div>
+                <div class="deck-listing">Deck 8</div>
+                <div class="deck-listing">Deck 9</div>
+                <div class="deck-listing">Deck 10</div>
                 </section>
        </div>
 
