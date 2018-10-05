ae65fed5810b6f3c309effbb0f8c04f7a4c8ce70
mth0348 <matthias.thomann@hotmail.com>
2018-10-05 14:11:21 +0200

    updated cardService.php to use a CardsMap instead of an array.

--- modules/services/cardService.php
+++ modules/services/cardService.php
@@ -2,7 +2,8 @@
     class CardService
     {
         private $baseUrl = 'https://api.hearthstonejson.com/v1/latest/enUS/cards.collectible.json'; 
-        private $allCards = array();
+        private $imageUrlFormat = "https://art.hearthstonejson.com/v1/render/latest/enUS/256x/{id}.png";
+        private $cardsMap = array();
 
         function __construct() {
             $this->downloadAllCards();
@@ -11,17 +12,33 @@
         private function downloadAllCards() {
             ini_set("allow_url_fopen", 1);
             $json = file_get_contents($this->baseUrl);
-            $this->allCards = json_decode($json);
+            $allCards = json_decode($json);
+
+            foreach ($allCards as $row) {
+                $row->imgLink = str_replace("{id}", $row->id, $this->imageUrlFormat);
+                $this->cardsMap[$row->id][] = $row;
+            }
         }
 
-        public function searchForCards($query)
+        public function searchForCard($query)
         {           
             $foundCards = array();
 
-            foreach($this->allCards as $card) {
-                if (strpos(strtolower($card->name), strtolower($query)) !== false) {
-                    array_push($foundCards, $card);
+            foreach($this->cardsMap as $card) {
+                if (strpos(strtolower($card[0]->name), strtolower($query)) !== false) {
+                    array_push($foundCards, $card[0]);
                 }
+            }
+
+            return $foundCards;
+        }
+
+        public function getCardsByDecklist($ids)
+        {           
+            $foundCards = array();
+
+            foreach($ids as $cardId) {
+                array_push($foundCards, $this->cardsMap[$cardId][0]);
             }
 
             return $foundCards;
