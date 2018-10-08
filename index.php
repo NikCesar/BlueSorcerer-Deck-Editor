5e02aff9c7f1bbca22fe77d9f623ac757e302adf
mth0348 <matthias.thomann@hotmail.com>
2018-10-08 19:17:39 +0200

    moved home.php to its own page.

--- index.php
+++ index.php
@@ -1,18 +1,8 @@
 <?php
     include "modules/requestHandler.php";
+    include "modules/helpers/contentRenderer.php";
 
-    $newestDecks = array(
-        (object) array("name" => "Temp burn aggro damage deck",
-              "description" => "My new super aggressive top tier deck! Check it out.",
-              "class" => "Hunter",
-              "score" => 92),
-        (object)  array("name" => "Rakdos Burn",
-              "description" => "I got gud, now look at my deck!",
-              "class" => "Mage",
-              "score" => -1),
-    );
-
-    $topDecks = $newestDecks;
+    $pageId = urldecode($_GET["page"]);
 ?>
 <!doctype html>
 <html>
@@ -23,31 +13,7 @@
     <body>
         <?php include "pages/partials/_topBar.php"; ?>
 
-       <div class="content">
-            <section id="newestDecks">
-                <input type="text" onkeyup="searchForCards(this.value);" />
-                
-                <?php foreach($newestDecks as $index=>$deck): ?>
-                    <div class="deck-listing">
-                        <h3><?php echo $deck->name ?></h3>
-                        <div class="description"><?php echo $deck->description ?></div>
-                        <div class="class"><?php echo $deck->class ?></div>
-                        <div class="score"><?php echo $deck->score ?></div>
-                    </div>
-                <?php endforeach; ?>
-            </section>
-    
-            <section id="topDecks">
-                <?php foreach($topDecks as $index=>$deck): ?>
-                    <div class="deck-listing">
-                        <h3><?php echo $deck->name ?></h3>
-                        <div class="description"><?php echo $deck->description ?></div>
-                        <div class="class"><?php echo $deck->class ?></div>
-                        <div class="score"><?php echo $deck->score ?></div>
-                    </div>
-                <?php endforeach; ?>
-            </section>
-       </div>
+       <?php renderContent($pageId); ?>
 
         <footer></footer>
     </body>
