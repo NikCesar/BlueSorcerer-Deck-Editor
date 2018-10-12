6b596071514ae766ae0644c47f5595ee46990439
mth0348 <matthias.thomann@hotmail.com>
2018-10-12 16:23:55 +0200

    Changed requires and includes for requestHandler.php.

--- modules/controllers/cardSearchController.php
+++ modules/controllers/cardSearchController.php
@@ -1,6 +1,4 @@
 <?php
-    require "$_SERVER[DOCUMENT_ROOT]/modules/services/cardService.php";
-
     $cardService = new CardService();
 
     if(isset($_GET['functionname']) && $_GET['functionname'] == "searchForCards"){
