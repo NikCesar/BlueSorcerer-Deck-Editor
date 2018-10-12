85824774565fb67889071a9156b1170312a6ae4c
mth0348 <matthias.thomann@hotmail.com>
2018-10-12 16:24:49 +0200

    Added modal div in _topBar.

--- pages/partials/_topBar.php
+++ pages/partials/_topBar.php
@@ -13,8 +13,43 @@
             <li><a href="#">Link 2</a></li>
             <li><a href="#">Link 3</a></li>
             <li><a href="#">Link 4</a></li>
-            <li id="loginItem"><a href="/?page=login">Login</a></li>
+            <?php if (isLoggedIn()) {
+                    echo '<li id="loginItem"><a href="/?page=userProfile">User profile</a></li>';
+                } else {
+                    echo '<li id="loginItem"><a href="/?page=login">Login</a></li>';
+                }
+            ?>
         </ul>
         <div style="clear: both"></div>
     </nav>
 </div>
+
+<div id="modal">
+    <div class="modal-content">
+    <div class="modal-header">
+        <span class="close" onclick="hideModal();">&times;</span>
+        <?php if (hasAnyMessage() && strpos(strtolower($_GET["message"]), "success") !== false) {
+                echo "<span class='success'>Success!</span>";
+            } else {
+                echo "<span class='fail'>Failed!</span>";
+            }
+        ?>
+    </div>
+    <div class="modal-body">
+        <p><?php 
+            if (hasAnyMessage()) {
+                echo text($_GET["message"]);
+            }
+        ?></p>
+    </div>
+    <div class="modal-footer">
+        <span>This window closes automatically...</span>
+    </div>
+    </div>
+</div>
+
+<?php 
+    if (hasAnyMessage()) {
+        echo '<script type="text/javascript">showModal();</script>';
+    }
+?>
