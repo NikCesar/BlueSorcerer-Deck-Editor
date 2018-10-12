eb8d1a9b9cfc7fb1f956e0884480caf25bdc1064
NikCesar <neonsugar420@gmail.com>
2018-10-12 14:33:25 +0200

    CardSearchByQueries javascript function

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,55 @@ function searchForCards(query) {
     }, 500);
 }
 
+function searchForCardsByQueries() {
+
+    var nameSearchValue = document.getElementById("cardName").value;
+    var ruleSearchValue = document.getElementById("cardText").value;
+    var costSearchValue = document.getElementById("cardCost").value;
+    var attackSearchValue = document.getElementById("cardAttack").value;
+    var healthSearchValue = document.getElementById("cardHealth").value;
+    var classSearchValue = document.getElementById("classSelect").value;
+    var typeSearchValue = document.getElementById("typeSelect").value;
+    var raceSearchValue = document.getElementById("raceSelect").value;
+    var setSearchValue = document.getElementById("setSelect").value;
+
+    var query = {};
+
+    if (nameSearchValue !== "") {
+        query['name'] = nameSearchValue;
+    }
+    if (ruleSearchValue !== "") {
+        query['text'] = ruleSearchValue;
+    }
+    if (costSearchValue !== "") {
+        query['cost'] = costSearchValue;
+    }
+    if (attackSearchValue !== "") {
+        query['attack'] = attackSearchValue;
+    }
+    if (healthSearchValue !== "") {
+        query['health'] = healthSearchValue;
+    }
+    if (classSearchValue !== "") {
+        query['playerClass'] = classSearchValue;
+    }
+    if (typeSearchValue !== "") {
+        query['type'] = typeSearchValue;
+    }
+    if (raceSearchValue !== "") {
+        query['race'] = raceSearchValue;
+    }
+    if (setSearchValue !== "") {
+        query['cardSet'] = setSearchValue;
+    }
+
+    var jsonRepresentation = JSON.stringify(query);
+
+    callPhpFunction("searchForCardsByQueries", "queries=" + jsonRepresentation, function (result) {
+        console.log(result);
+    });
+}
+
 // #region Helper methods
 function callPhpFunction(functionName, param, successCallback) {
     $.ajax({
