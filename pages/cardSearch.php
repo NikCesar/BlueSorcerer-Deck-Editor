4a2a8bf5b7ef80e83410af7c22ffd14fb8d493b9
NikCesar <neonsugar420@gmail.com>
2018-10-18 14:51:04 +0200

    cardSearch now displays the searched cards as images.

--- pages/cardSearch.php
+++ pages/cardSearch.php
@@ -1,117 +1,119 @@
 <div class="content">
     <section id="cardFilter">
-        <div>
-                    <label><?php echo text("CardSearchName"); ?></label>
-            <input type="text" id="cardName" />
-        </div>
+        <form action="" method="POST">
+            <input type="text" name="functionname" value="searchForCardsByQueries" style="display:none">
 
-        <div>
-            <label><?php echo text("CardSearchRule"); ?></label>
-            <input type="text" id="cardText" />
-        </div>
+            <input type="text" name="page" value="cardSearch" style="display: none">
 
-        <div>
-            <label><?php echo text("CardSearchCost"); ?></label>
-            <input type="number" id="cardCost" />
-        </div>
+            <div>
+                <label><?php echo text("CardSearchName"); ?></label>
+                <input type="text" name="cardName" id="cardName"/>
+            </div>
 
-        <div>
-                    <label><?php echo text("CardSearchAttack"); ?></label>
-                    <input type="number" id="cardAttack" />
-                </div>
+            <div>
+                <label><?php echo text("CardSearchRule"); ?></label>
+                <input type="text" name="cardText" id="cardText"/>
+            </div>
 
-                <div>
-                    <label><?php echo text("CardSearchHealth"); ?></label>
-                    <input type="number" id="cardHealth" />
-                </div>
+            <div>
+                <label><?php echo text("CardSearchCost"); ?></label>
+                <input type="number" name="cardCost" id="cardCost"/>
+            </div>
 
-                <div>
-            <label><?php echo text("CardSearchClass"); ?></label>
-            <select id="classSelect">
-                <option value=""></option>
-                <option value="Druid"><?php echo text("ClassDruid"); ?></option>
-                <option value="Hunter"><?php echo text("ClassHunter"); ?></option>
-                <option value="Mage"><?php echo text("ClassMage"); ?></option>
-                <option value="Paladin"><?php echo text("ClassPaladin"); ?></option>
-                <option value="Priest"><?php echo text("ClassPriest"); ?></option>
-                <option value="Rogue"><?php echo text("ClassRogue"); ?></option>
-                <option value="Shaman"><?php echo text("ClassShaman"); ?></option>
-                <option value="Warlock"><?php echo text("ClassWarlock"); ?></option>
-            </select>
-        </div>
+            <div>
+                <label><?php echo text("CardSearchAttack"); ?></label>
+                <input type="number" name="cardAttack" id="cardAttack"/>
+            </div>
 
-        <div>
-            <label><?php echo text("CardSearchType"); ?> </label>
-            <select id="typeSelect" onchange="raceSelectEnabled()">
-                        <option value="">-</option>
-                        <option value="Weapon"><?php echo text("CardTypeWeapon"); ?></option>
-                        <option value="Minion"><?php echo text("CardTypeMinion"); ?></option>
-                        <option value="Spell"><?php echo text("CardTypeSpell"); ?></option>
-                        <option value="Hero"><?php echo text("CardTypeHero"); ?></option> <!-- collectible == true, else we also get the unplayable heroes -->
-            </select>
-        </div>
+            <div>
+                <label><?php echo text("CardSearchHealth"); ?></label>
+                <input type="number" name="cardHealth" id="cardHealth"/>
+            </div>
 
-        <div id="raceSelectDiv" style="display: none">
-                    <label><?php echo text("CardSearchRace"); ?> </label>
-            <select id="raceSelect">
-                        <option value="">-</option>
-                        <option value="Beast"><?php echo text("CardRaceBeast"); ?></option>
-                        <option value="Demon"><?php echo text("CardRaceDemon"); ?></option>
-                        <option value="Dragon"><?php echo text("CardRaceDragon"); ?></option>
-                        <option value="Elemental"><?php echo text("CardRaceElemental"); ?></option>
-                        <option value="Mech"><?php echo text("CardRaceMech"); ?></option>
-                        <option value="Murloc"><?php echo text("CardRaceMurloc"); ?></option>
-                        <option value="Pirate"><?php echo text("CardRacePirate"); ?></option>
-                        <option value="Totem"><?php echo text("CardRaceTotem"); ?></option>
-            </select>
-        </div>
+            <div>
+                <label><?php echo text("CardSearchClass"); ?></label>
+                <select name="classSelect" id="classSelect">
+                    <option value=""></option>
+                    <option value="Druid"><?php echo text("ClassDruid"); ?></option>
+                    <option value="Hunter"><?php echo text("ClassHunter"); ?></option>
+                    <option value="Mage"><?php echo text("ClassMage"); ?></option>
+                    <option value="Paladin"><?php echo text("ClassPaladin"); ?></option>
+                    <option value="Priest"><?php echo text("ClassPriest"); ?></option>
+                    <option value="Rogue"><?php echo text("ClassRogue"); ?></option>
+                    <option value="Shaman"><?php echo text("ClassShaman"); ?></option>
+                    <option value="Warlock"><?php echo text("ClassWarlock"); ?></option>
+                </select>
+            </div>
 
-        <div>
-                    <label><?php echo text("CardSearchSet"); ?> </label>
-            <select id="setSelect">
-                        <option value="">-</option>
-                        <option value="CORE"><?php echo text("CardSetBasic"); ?></option>
-                        <option value="Classic"><?php echo text("CardSetClassic"); ?></option>
-                        <option value="Hall of Fame"><?php echo text("CardSetHoF"); ?></option>
-                        <option value="Naxxramas"><?php echo text("CardSetNaxx"); ?></option>
-                        <option value="GVG"><?php echo text("CardSetGvG"); ?></option>
-                        <option value="Blackrock Mountain"><?php echo text("CardSetBRM"); ?></option>
-                        <option value="TGT"><?php echo text("CardSetTgT"); ?></option>
-                        <option value="LOE"><?php echo text("CardSetLoE"); ?></option>
-                        <option value="OG"><?php echo text("CardSetWOG"); ?></option>
-                        <option value="One Night in Karazhan"><?php echo text("CardSetKara"); ?></option>
-                        <option value="Mean Streets of Gadgetzan"><?php echo text("CardSetMSG"); ?></option>
-                        <option value="UNGORO"><?php echo text("CardSetUnGoro"); ?></option>
-                        <option value="Knights of the Frozen Throne"><?php echo text("CardSetKFT"); ?></option>
-                        <option value="Kobolds & Catacombs"><?php echo text("CardSetKaC"); ?></option>
-                        <option value="The Witchwood"><?php echo text("CardSetWood"); ?></option>
-                        <option value="BOOMSDAY"><?php echo text("CardSetBdP"); ?></option>
-            </select>
-        </div>
+            <div>
+                <label><?php echo text("CardSearchType"); ?> </label>
+                <select name="typeSelect" id="typeSelect" onchange="raceSelectEnabled()">
+                    <option value="">-</option>
+                    <option value="Weapon"><?php echo text("CardTypeWeapon"); ?></option>
+                    <option value="Minion"><?php echo text("CardTypeMinion"); ?></option>
+                    <option value="Spell"><?php echo text("CardTypeSpell"); ?></option>
+                    <option value="Hero"><?php echo text("CardTypeHero"); ?></option>
+                    <!-- collectible == true, else we also get the unplayable heroes -->
+                </select>
+            </div>
 
-                <input type="submit" value="<?php echo text("CardSearchSubmit"); ?>" onclick="searchForCardsByQueries()"/>
+            <div id="raceSelectDiv" style="display: none">
+                <label><?php echo text("CardSearchRace"); ?> </label>
+                <select name="raceSelect" id="raceSelect">
+                    <option value="">-</option>
+                    <option value="Beast"><?php echo text("CardRaceBeast"); ?></option>
+                    <option value="Demon"><?php echo text("CardRaceDemon"); ?></option>
+                    <option value="Dragon"><?php echo text("CardRaceDragon"); ?></option>
+                    <option value="Elemental"><?php echo text("CardRaceElemental"); ?></option>
+                    <option value="Mech"><?php echo text("CardRaceMech"); ?></option>
+                    <option value="Murloc"><?php echo text("CardRaceMurloc"); ?></option>
+                    <option value="Pirate"><?php echo text("CardRacePirate"); ?></option>
+                    <option value="Totem"><?php echo text("CardRaceTotem"); ?></option>
+                </select>
+            </div>
+
+            <div>
+                <label><?php echo text("CardSearchSet"); ?> </label>
+                <select name="setSelect" id="setSelect">
+                    <option value="">-</option>
+                    <option value="CORE"><?php echo text("CardSetBasic"); ?></option>
+                    <option value="EXPERT1"><?php echo text("CardSetClassic"); ?></option>
+                    <option value="HOF"><?php echo text("CardSetHoF"); ?></option>
+                    <option value="NAXX"><?php echo text("CardSetNaxx"); ?></option>
+                    <option value="GVG"><?php echo text("CardSetGvG"); ?></option>
+                    <option value="BRM"><?php echo text("CardSetBRM"); ?></option>
+                    <option value="TGT"><?php echo text("CardSetTgT"); ?></option>
+                    <option value="LOE"><?php echo text("CardSetLoE"); ?></option>
+                    <option value="OG"><?php echo text("CardSetWOG"); ?></option>
+                    <option value="KARA"><?php echo text("CardSetKara"); ?></option>
+                    <option value="GANGS"><?php echo text("CardSetMSG"); ?></option>
+                    <option value="UNGORO"><?php echo text("CardSetUnGoro"); ?></option>
+                    <option value="ICECROWN"><?php echo text("CardSetKFT"); ?></option>
+                    <option value="LOOTAPALOOZA"><?php echo text("CardSetKaC"); ?></option>
+                    <option value="GILNEAS"><?php echo text("CardSetWood"); ?></option>
+                    <option value="BOOMSDAY"><?php echo text("CardSetBdP"); ?></option>
+                </select>
+            </div>
+
+            <input type="submit" value="<?php echo text("CardSearchSubmit"); ?>"/>
+        </form>
     </section>
 
-    <hr />
+    <hr/>
 
-    <p>Card 1</p>
-    <p>Card 2</p>
-    <p>Card 3</p>
-    <p>Card 4</p>
-    <p>Card 5</p>
-    <p>Card 6</p>
-    <p>Card 7</p>
-    <p>Card 8</p>
-    <p>Card 9</p>
-    <p>Card 10</p>
-    <p>Card 1</p>
-    <p>Card 2</p>
-    <p>Card 3</p>
-    <p>Card 4</p>
-    <p>Card 5</p>
-    <p>Card 6</p>
-    <p>Card 7</p>
-    <p>Card 8</p>
-    <p>Card 9</p>
-    <p>Card 10</p>
+    <section id="searchedCards">
+        <h3>Result</h3>
+        <?php if (isset($_SESSION["searchResult"])): ?>
+            <?php $searchResult = $_SESSION["searchResult"];
+            foreach ($searchResult as $card): ?>
+                <?php foreach ($card as $key => $value): ?>
+                    <?php if ($key === "imgLink"): ?>
+                        <div class="displayedCard">
+                            <img src="<?php echo $value ?>" >
+                        </div>
+                    <?php endif; ?>
+                <?php endforeach; ?>
+            <?php endforeach; ?>
+        <?php endif; ?>"
+    </section>
 </div>
