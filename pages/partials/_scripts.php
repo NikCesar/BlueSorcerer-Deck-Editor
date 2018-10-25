7852867383ffcef7f90f5f6b5c63675f87fc62b4
NikCesar <neonsugar420@gmail.com>
2018-10-25 16:34:19 +0200

    tooltipster script added, but commented out bc not working yet

--- pages/partials/_scripts.php
+++ pages/partials/_scripts.php
@@ -4,5 +4,6 @@
 
 <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
 <script src="http://<?php echo $_SERVER['SERVER_NAME']; ?>/assets/js/script.js"></script>
+<!--<script src="http://--><?php //echo $_SERVER['SERVER_NAME']; ?><!--/assets/js/tooltipster.bundle.js/tooltipster.bundle.min.js"></script>-->
 
 <?php require "$_SERVER[DOCUMENT_ROOT]/assets/values/languages.php"; ?>
