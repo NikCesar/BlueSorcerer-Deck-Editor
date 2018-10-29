4b36115d7e8105662f717d6ee7a890c011eaaa12
mth0348 <matthias.thomann@hotmail.com>
2018-10-29 12:36:30 +0100

    Added createDeck section in decksOverview.

--- pages/decksOverview.php
+++ pages/decksOverview.php
@@ -29,4 +29,42 @@
         <?php endforeach; ?>
     </section>
 
+    <hr/>
+
+    <section id="createDeck">
+        <h1><?php echo text("CreateDeck"); ?></h1>
+
+        <form id="createDeckForm" action="" method="POST">
+            <input type="text" name="functionname" value="addDeck" class="hidden">
+            <input type="text" name="page" value="deckEditor" class="hidden">
+
+            <div>
+                <label><?php echo text("DeckName"); ?></label>
+                <input type="text" name="deckName" />
+            </div>
+            <div>
+                <label><?php echo text("DeckDescription"); ?></label>
+                <textarea type="text" name="deckDescription" form="editDeckForm"></textarea>
+            </div>
+            <div>
+                <label><?php echo text("DeckClass"); ?></label>
+                <select name="deckClass">
+                    <option value=""></option>
+                    <option value="Druid"><?php echo text("ClassDruid"); ?></option>
+                    <option value="Hunter"><?php echo text("ClassHunter"); ?></option>
+                    <option value="Mage"><?php echo text("ClassMage"); ?></option>
+                    <option value="Paladin"><?php echo text("ClassPaladin"); ?></option>
+                    <option value="Priest"><?php echo text("ClassPriest"); ?></option>
+                    <option value="Rogue"><?php echo text("ClassRogue"); ?></option>
+                    <option value="Shaman"><?php echo text("ClassShaman"); ?></option>
+                    <option value="Warlock"><?php echo text("ClassWarlock"); ?></option>
+                </select>
+            </div>
+
+            <?php echo validationMessageFor("creationFailed") ?>
+
+            <input type="submit" value="<?php echo text("Create"); ?>" />            
+        </form>
+    </section>
+
 </div>
