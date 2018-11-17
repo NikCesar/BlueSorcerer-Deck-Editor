5baebcc8c55e672df1ca8334307887ef532298d6
NikCesar <neonsugar420@gmail.com>
2018-11-17 12:37:37 +0100

    renderWithAddLink function to print the searchResult in the deckEditor

--- modules/helpers/cardSearchResult.php
+++ modules/helpers/cardSearchResult.php
@@ -6,7 +6,8 @@
  * Time: 19:30
  */
 
-class CardSearchResult {
+class CardSearchResult
+{
 
     private $searchResult;
 
@@ -25,5 +26,21 @@ class CardSearchResult {
         }
 
     }
+
+    function renderWithAddLink($deck)
+    {
+        foreach ($this->searchResult as $card) {
+            echo "<div class=\"displayedCard hover-plus\">";
+            echo "    <form id=\"add_<?php echo $card->id ?>\" action=\"\" method=\"POST\">";
+            echo "        <input type=\"text\" name=\"functionname\" value=\"addCard\" class=\"hidden\" />";
+            echo "        <input type=\"text\" name=\"cardId\" value=\"$card->id\" class=\"hidden\" />";
+            echo "        <input type=\"text\" name=\"deckId\" value=\"$deck->Id\" class=\"hidden\" />";
+            echo "        <input type=\"submit\" value=\"+\" onclick=\"maintainScrollPos();\" class=\"add-card\" />";
+            echo "    </form>";
+            echo "    <img src=\"" . getCardImgLink($card->id) . "\">";
+            echo "</div>";
+        }
+    }
+
 
 }
