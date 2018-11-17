60763f32389018055d5c6b3f866258c0011465b6
NikCesar <neonsugar420@gmail.com>
2018-11-17 12:37:12 +0100

    searchResult moved to the globals, so it can be accessed correctly

--- modules/controllers/cardSearchController.php
+++ modules/controllers/cardSearchController.php
@@ -42,6 +42,6 @@
             $queries['set'] = strip_tags($_POST['setSelect']);
         }
         $queries['collectible'] = true;
-        $searchResult = new CardSearchResult($cardService->searchForCardsByQueries($queries));
+        $GLOBALS['cardSearchResult'] = new CardSearchResult($cardService->searchForCardsByQueries($queries));
     }
 ?>
