4e772a88151e89860f4eaa6067978446b828f387
mth0348 <matthias.thomann@hotmail.com>
2018-12-10 17:33:56 +0100

    cleaned index.php. Renamed a lot of functions in favor of better understanding what happens at the start of each request.

--- index.php
+++ index.php
@@ -4,43 +4,86 @@
     include "modules/requestHandler.php";
     include "modules/helpers/contentRenderer.php";
 
-    if (!isset($_SESSION))
-    {
-        session_start();
+    ensureSessionStartAndSetLanguage();
 
-        $_SESSION["language"] = "en";
-    } else if (!array_key_exists("language", $_SESSION)) {
-        $_SESSION["language"] = "en";
-    }
+    // declare exception rulesets.
+    $noHtmlCalls = array();
+    setupRoutingExceptions();
 
     // redirect to controller and action.
-    $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-    $components = explode('/', $path);
+    $requestUrl = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+    $controller = "";
+    $action = "";
+    getControllerAndActionFromRequestUrl();
+    
+    // try resolve controller.
+    $controllerInstance = null;
+    resolveControllerInstance();
 
-    if ($path == "/favicon.ico") return;
+    // decide wether or not the path controller/action is a JS call. If so, render without html/body.
+    applyRountingExceptions();
+?>
 
-    if (count($components) == 2) {
-        if (empty($components[1])) {
-            $components[1] = "Home";
+<?php 
+    /**
+     * Private functions are here.
+     */
+
+    function ensureSessionStartAndSetLanguage() {
+        if (!isset($_SESSION))
+        {
+            session_start();
+        } 
+        
+        if (!array_key_exists("language", $_SESSION)) {
+            $_SESSION["language"] = "en";
         }
-        $controller = $components[1] . "Controller";
-    } else if (count($components) == 3) {
-        $controller = $components[1] . "Controller";
-        $action = $components[2];
-    } else {
-        $controller = "HomeController";
-        $action = "index";
     }
 
-    try {
-        $controllerInstance = new $controller();
-    } catch (\Throwable $th) {
-        redirect("home", "notFound", "page=" . $components[1]);
+    function setupRoutingExceptions() {
+        array_push($noHtmlCalls, "deckEditor/getJsDeck");
     }
 
-    if (!isset($action) || empty($action) || !method_exists($controllerInstance, $action)) {
-        if (property_exists($controllerInstance, "defaultAction")) {
-            $action = $controllerInstance->defaultAction;
+    function getControllerAndActionFromRequestUrl() {
+        $components = explode('/', $requestUrl);
+
+        // set controller and action from path, if possible.
+        if (count($components) == 2) {
+            if (empty($components[1])) {
+                $components[1] = "Home";
+            }
+            $controller = $components[1] . "Controller";
+        } else if (count($components) == 3) {
+            $controller = $components[1] . "Controller";
+            $action = $components[2];
+        } else {
+            $controller = "HomeController";
+            $action = "index";
+        }
+
+        // try to get default action if none was specified.
+        if (!isset($action) || empty($action) || !method_exists($controllerInstance, $action)) {
+            if (property_exists($controllerInstance, "defaultAction")) {
+                $action = $controllerInstance->defaultAction;
+            }
+        }
+    }
+
+    function resolveControllerInstance() {
+        try {
+            if ($path == "/favicon.ico") return;
+            
+            $controllerInstance = new $controller();
+        } catch (\Throwable $th) {
+            redirect("home", "notFound", "page=" . $components[1]);
+        }
+    }
+
+    function applyRountingExceptions() {
+        // call action without rendering any HTML. This ensures that only the return value of the action is really rendered. Used for JavaScript HTTP GET calls.
+        if (in_array("$controller/$action", $noHtmlCalls)) {
+            $controllerInstance->{$action}();
+            exit;
         }
     }
 ?>
