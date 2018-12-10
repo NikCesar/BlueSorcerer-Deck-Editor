f460a2d4c0ca9293058d8743ace54357ec21d16a
mth0348 <matthias.thomann@hotmail.com>
2018-12-10 18:40:13 +0100

    moved login userProfile.php to UserProfileController + Model + View.

--- modules/controllers/userProfileController.php
+++ modules/controllers/userProfileController.php
@@ -1,7 +1,31 @@
 <?php
-    $dbService = new DbService();
 
-    if(isset($_GET['functionname']) && $_GET['functionname'] == "logout"){
+class UserProfileController {
+
+    private $userProfileModel;
+    private $userProfileView;
+
+    function __construct() {
+        $this->userProfileModel = new UserProfileModel();
+        $this->userProfileView = new UserProfileView();
+    }
+
+    /** @view method */
+    public function index() {
+        $this->userProfileView->renderUserProfile();
+    }
+
+    public function updateUserProfile() {
+        $id = $_SESSION["user"]->Id;
+        $username = strip_tags($_POST["Username"]);
+        $email = strip_tags($_POST["Email"]);
+
+        $updatedUser = $this->userProfileModel->updateUser($id, $username, $email);
+
+        $_SESSION["user"] = $updatedUser;
+    }
+
+    public function logout() {
         if (isLoggedIn()) {
             $_SESSION["user"] = null;
             $_SESSION["isLoggedIn"] = false;
@@ -10,13 +34,5 @@
         }
     }
 
-    if(isset($_POST['functionname']) && $_POST['functionname'] == "saveUserProfile"){
-        $id = $_SESSION["user"]->Id;
-        $username = strip_tags($_POST["Username"]);
-        $email = strip_tags($_POST["Email"]);
-
-        $updatedUser = $dbService->updateUser($id, $username, $email);
-
-        $_SESSION["user"] = $updatedUser;
-    }
+}
 ?>
---
+++ modules/view/userProfileView.php
@@ -0,0 +1,44 @@
+<?php
+class LoginView {
+
+    private $isEditMode;
+
+    public function __construct()
+    {
+        $this->isEditMode = (isset($_POST["isEditMode"]) && $_POST["isEditMode"]) ? "isEditMode" : "isReadMode";
+    }
+
+    public function renderLogin() {
+            echo "<section class=\"" . $this->isEditMode . "\">";
+            echo "<form action=\"/userProfile/saveUserProfile\" method=\"POST\">";
+            echo "    <div>";
+            echo "        <label>Id: </label>";
+            echo "        <label class=\"readMode\">" . $_SESSION["user"]->Id . "</label>";
+            echo "        <input class=\"editMode\" type=\"text\" name=\"Id\" value=\"" . $_SESSION["user"]->Id . "\" disabled=\"disabled\" />";
+            echo "    </div>";
+            echo "    <div>";
+            echo "        <label>Username: </label>";
+            echo "        <label class=\"readMode\">" . $_SESSION["user"]->Username . "</label>";
+            echo "        <input class=\"editMode\" type=\"text\" name=\"Username\" value=\"" . $_SESSION["user"]->Username . "\" />";
+            echo "    </div>";
+            echo "    <div>";
+            echo "        <label>Email: </label>";
+            echo "        <label class=\"readMode\">" . $_SESSION["user"]->Email . "</label>";
+            echo "        <input class=\"editMode\" type=\"text\" name=\"Email\" value=\"" . $_SESSION["user"]->Email . "\" />";
+            echo "    </div>";
+
+            echo "    <input class=\"editMode\" type=\"button\" value=\"Cancel\" onclick=\"window.location.href = window.location.href;\" />";
+            echo "    <input class=\"editMode\" type=\"submit\" value=\"Save\" />";
+            echo "</form>";
+
+            echo "<form action=\"/userProfile\" method=\"POST\">";
+            echo "    <input class=\"readMode hidden\" type=\"text\" name=\"isEditMode\" value=\"true\" />";
+            echo "    <input class=\"readMode\" type=\"submit\" value=\"Edit\" />";
+            echo "</form>";
+
+            echo "<form action=\"/userProfile/logout\" method=\"GET\">";
+            echo "    <input type=\"submit\" value=\"Logout\" />";
+            echo "</form>";
+            echo "</section>";
+    }   
+}
