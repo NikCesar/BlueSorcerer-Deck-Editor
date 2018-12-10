33e006dcd928aa4fa400251bc4a35bc3d996d0d5
mth0348 <matthias.thomann@hotmail.com>
2018-12-10 16:18:39 +0100

    Moved login.php to LoginController, view and model and removed old page afterwards.

--- modules/controllers/loginController.php
+++ modules/controllers/loginController.php
@@ -1,20 +1,41 @@
 <?php
-    $dbService = new DbService();
+require_once ("$_SERVER[DOCUMENT_ROOT]/modules/view/loginView.php");
+require_once ("$_SERVER[DOCUMENT_ROOT]/modules/models/loginModel.php");
 
-    if(isset($_POST['functionname']) && $_POST['functionname'] == "login"){
+class LoginController {
+
+    private $loginModel;
+    private $loginView;
+    private $redirectTo;
+
+    public $defaultAction = "showLogin";
+
+    function __construct() {
+        $this->loginModel = new LoginModel();
+        $this->loginView = new LoginView();
+    }
+
+    /** @view method */
+    public function showLogin() {
+        $this->loginView->renderLogin();
+    }
+
+    public function doLogin() {
         $username = $_POST['username'];
         $password = $_POST['password'];
 
-        $dbUser = $dbService->getUserByUsername($username);
+        $dbUser = $this->loginModel->getUserByUsername($username);
 
-        $isLoginValid = verifyLogin($username, $password, $dbUser);
+        $isLoginValid = $this->verifyLogin($username, $password, $dbUser);
 
         if ($isLoginValid) {
             $_SESSION["user"] = $dbUser;
             $_SESSION["isLoggedIn"] = true;
 
-            if (isset($_GET["redirect"])) {
-                redirect($_GET["redirect"]);
+            if (isset($_SESSION["redirectTo"])) {
+                $redirectTo = $_SESSION["redirectTo"];
+                $_SESSION["redirectTo"] = null;
+                redirect($redirectTo);
             }
 
             redirect("home", "message=loginSuccess");
@@ -23,10 +44,11 @@
         }
     }
 
-    function verifyLogin($username, $password, $dbUser) {
+    private function verifyLogin($username, $password, $dbUser) {
         if ($dbUser === null) {
             return false;
         }
         return password_verify($password, $dbUser->Password);
     }
+}
 ?>
