0a760d25ba5ae8a4bce10122ff55c319a9cede66
mth0348 <matthias.thomann@hotmail.com>
2018-12-10 18:55:26 +0100

    Added missing translations.

--- assets/values/languages.php
+++ assets/values/languages.php
@@ -81,6 +81,8 @@
         "NoDeckFound" => array("en" => "No deck found with this id.", "de" => "Es wurde kein Deck mit dieser Id gefunden."),
         "NotYourDeck" => array("en" => "You do not have permission to edit this deck.", "de" => "Sie haben keine Berechtigung, dieses Deck zu bearbeiten."),
         "createDeckFail" => array("en" => "Failed to create a new deck. Ensure that both name and class are set.", "de" => "Erstellen des Decks ist fehlgeschlagen. Stellen Sie sicher, dass Name und Klasse ausgefüllt sind."),
+        "DeckName" => array("en" => "Deck name", "de" => "Deck Name"),
+        "DeckDescription" => array("en" => "Deck description", "de" => "Deck Beschreibung"),
     );
 
     function text($key){
