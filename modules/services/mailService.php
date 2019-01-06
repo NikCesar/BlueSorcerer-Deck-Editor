ce4a40a6dec01c1b2f1bbbfec40c3d80b7278d13
mth0348 <matthias.thomann@hotmail.com>
2019-01-06 17:01:46 +0100

    Added a mailService.

---
+++ modules/services/mailService.php
@@ -0,0 +1,56 @@
+<?php
+
+require_once "$_SERVER[DOCUMENT_ROOT]/modules/services/dbService.php";
+
+class MailService
+{
+   private $dbService;
+   private $passwordResetEmailSubject;
+   private $passwordResetEmailBody;
+
+   function __construct() {
+      $this->dbService = new DbService();
+   }
+
+   function sendPasswordResetEmail($userId, $email) 
+   {
+      $this->passwordResetEmailSubject = text("passwordResetEmailSubject");
+      $this->passwordResetEmailBody = text("passwordResetEmailBody");
+   
+      if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+         return false;
+      }
+
+      // Generate token and link.
+      $resetToken = $this->generateToken();
+
+      // Save reset token.
+      $this->dbService->saveResetToken($userId, $resetToken);
+
+      // Create password reset link.
+      $host = "http://bluesorcerer.epizy.com";
+      $passwordResetLink = "{$host}/admin/passwordreset?token=" . $resetToken;
+
+       // update mail text with link and add separator at the end.
+      $this->passwordResetEmailBody = str_replace("{{link}}", "<a href=\"".$passwordResetLink."\" />", $this->passwordResetEmailBody);
+      $this->passwordResetEmailBody = $this->passwordResetEmailBody."\n\n\n<hr/>";
+
+      $headers = "MIME-Version: 1.0" . "\r\n";
+      $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+      $headers .= 'From: system@bluesorcerer.com' . "\r\n";
+
+      mail($email, $this->passwordResetEmailSubject, $this->passwordResetEmailBody, $headers);
+
+      return true;
+   }
+
+   function generateToken()
+   {
+      if (function_exists('com_create_guid') === true)
+      {
+         return trim(com_create_guid(), '{}');
+      }
+
+      return sprintf('%04X%04X-%04X-%04X-%04X-%04X%04X%04X', mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(16384, 20479), mt_rand(32768, 49151), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535));
+   }
+}
