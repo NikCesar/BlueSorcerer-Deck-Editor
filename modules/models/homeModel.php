54295e59d417d98955be6e5149a80c5a5f0ab6d1
NikCesar <neonsugar420@gmail.com>
2019-01-12 13:26:43 +0100

    The date is displayed in the european format

--- modules/models/homeModel.php
+++ modules/models/homeModel.php
@@ -8,6 +8,10 @@ class HomeModel {
         $this->dbService = new DbService();
 
         $this->newestDecks = $this->dbService->getNewestPublishedDecks();
+        foreach($this->newestDecks as $key=>$deck) {
+            $formatedDate = date_create_from_format("Y-m-d H:i:s", $deck->PublishDate);
+            $this->newestDecks[$key]->PublishDate = date_format($formatedDate, "d-m-Y H:i:s");
+        }
     } 
 }
 ?>
